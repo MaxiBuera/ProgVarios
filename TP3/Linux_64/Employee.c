22d3d5cf571f9e99b55a02ad74c5fe35eb8abfab
MaxiBuera <43425039+MaxiBuera@users.noreply.github.com>
2018-11-15 20:27:02 -0300

    Add files via upload

--- TP3/Linux_64/Employee.c
+++ TP3/Linux_64/Employee.c
@@ -1,3 +1,149 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Employee.h"
 
+static int isValidNombre(char* nombre);
+
+
+Employee* employee_new()
+{
+
+    return malloc(sizeof(Employee));
+}
+
+Employee* employee_newParametros(char* idStr, char* nombreStr, char* horasTrabajadasStr)
+{
+    Employee* this;
+    this = employee_new();
+    int id;
+    char nombre[128];
+    int horasTrabajadas;
+
+    //Si ya valide
+
+    id=atoi(idStr);
+    strcpy(nombre,nombreStr);
+    horasTrabajadas = atoi(horasTrabajadasStr);
+
+    empleados_setId(this,id);
+    empleados_setNombre(this,nombre);
+    empleados_setHorasTrabajadas(this,horasTrabajadas);
+
+    return this;
+}
+
+void empleados_delete(Employee* this)
+{
+    free(this);
+}
+
+int empleados_setNombre(Employee* this, char* nombre)
+{
+    int retorno = -1;
+    if(this != NULL && nombre != NULL && isValidNombre(nombre))
+    {
+        retorno = 0;
+        strcpy(this->nombre,nombre);
+    }
+    return retorno;
+}
+
+int empleados_getNombre(Employee* this, char* nombre)
+{
+    int retorno = -1;
+    if(this != NULL && nombre != NULL)
+    {
+        retorno = 0;
+        strcpy(nombre,this->nombre);
+    }
+    return retorno;
+}
+
+int empleados_setId(Employee* this, int id)
+{
+    int retorno = -1;
+    static int maximoId = -1;
+    if(this != NULL)
+    {
+        retorno = 0;
+        if(id >= 0)
+        {
+
+            if(id > maximoId)
+                maximoId = id;
+            this->id = id;
+        }
+        else
+        {
+            maximoId++;
+            this->id = maximoId;
+        }
+
+    }
+
+    return retorno;
+}
+
+int empleados_getId(Employee* this, int* id)
+{
+    int retorno = -1;
+    if(this != NULL && id != NULL)
+    {
+        retorno = 0;
+        *id = this->id;
+    }
+    return retorno;
+}
+
+int empleados_setHorasTrabajadas(Employee* this, int horasTrabajadas)
+{
+    int retorno = -1;
+    if(this != NULL)
+    {
+        retorno = 0;
+        this->horasTrabajadas = horasTrabajadas;
+    }
+    return retorno;
+}
+
+int empleados_getHorasTrabajadas(Employee* this, int* horasTrabajadas)
+{
+    int retorno = -1;
+    if(this != NULL && horasTrabajadas != NULL)
+    {
+        retorno = 0;
+        *horasTrabajadas = this->horasTrabajadas;
+    }
+    return retorno;
+}
+
+int empleados_setSueldo(Employee* this, int sueldo)
+{
+    int retorno = -1;
+    if(this != NULL)
+    {
+        retorno = 0;
+        this->sueldo = sueldo;
+    }
+    return retorno;
+}
+
+int empleados_getSueldo(Employee* this, int* sueldo)
+{
+    int retorno = -1;
+    if(this != NULL && sueldo != NULL)
+    {
+        retorno = 0;
+        *sueldo = this->sueldo;
+    }
+    return retorno;
+}
+
+
+static int isValidNombre(char* nombre)
+{
+    return 1;
+}
 
 
--- TP3/Linux_64/parser.c
+++ TP3/Linux_64/parser.c
@@ -13,7 +13,41 @@
 int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
 {
 
-    return 1;
+    int id[4096];
+    char nombre[4096];
+    char horasTrabajadas[4096];
+
+    Employee* auxEmployee;
+
+
+    int auxInt;
+
+    //int id2;
+
+    //int horasTrabajadas;
+
+    pFile = NULL;
+    pFile = fopen("data.csv","r");
+    char nombre2[256];
+    if(pFile != NULL && pArrayListEmployee != NULL){
+
+        while(!feof(pFile)){ // == 0
+
+            fscanf(pFile,"%[^,],%[^,],%[^,\n]\n",id,nombre,horasTrabajadas);
+            //printf("\n%s - %s - %s\n",nombre,horasTrabajadas);
+            auxEmployee = employee_newParametros(id,nombre,horasTrabajadas);
+            ll_add(pArrayListEmployee,auxEmployee);
+         /* uxEmpleados = empleados_new();
+            empleados_setNombre(auxEmpleados,nombre);
+            empleados_setHorasTrabajadas(auxEmpleados,horasTrabajadas2);
+            al_add(listaEmpleados,auxEmpleados);
+            auxInt = al_len(listaEmpleados);*/
+        }
+        //printf("%d\n",auxInt);
+
+    }
+    fclose(pFile);
+    return 1; // OK
 }
 
 /** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
@@ -28,3 +62,4 @@ int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee)
 
     return 1;
 }
+
